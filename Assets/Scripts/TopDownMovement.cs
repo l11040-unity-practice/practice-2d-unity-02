0647348c117dcd5fd1315848480761311f0f0d8b
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-08 19:32:53 +0900

    Update : Input System으로 입력 업그레이드

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownController controller;
+    private Rigidbody2D movementRigidbody;
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        //주로 내 컴포넌트에서 끝나는 행위
+        controller = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+    private void FixedUpdate()
+    {
+        //물리 업데이트 관련 작성
+        ApplyMovement(movementDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        movementRigidbody.velocity = direction;
+    }
+}
