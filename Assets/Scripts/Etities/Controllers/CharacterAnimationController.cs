2e085324986f7e5680649b54bd4380dc56b6e81a
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-09 14:45:11 +0900

    Feature : 애니메이션 컨트롤

---
+++ Assets/Scripts/Etities/Controllers/CharacterAnimationController.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class CharacterAnimationController : AnimationController
+{
+    private static readonly int isWalking = Animator.StringToHash("isWalking");
+    private static readonly int isHit = Animator.StringToHash("isHit");
+    private static readonly int attack = Animator.StringToHash("attack");
+
+    private readonly float magnituteThreshold = 0.5f;
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        controller.OnAttackEvent += Attacking;
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 vector)
+    {
+        animator.SetBool(isWalking, vector.magnitude > magnituteThreshold);
+    }
+
+    private void Attacking(AttackSO attackSO)
+    {
+        animator.SetTrigger(attack);
+    }
+    private void Hit()
+    {
+        animator.SetBool(isHit, true);
+    }
+
+    private void InvincibilityEnd()
+    {
+        animator.SetBool(isHit, false);
+    }
+}
